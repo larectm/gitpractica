ed3dae497cd379e38607faff51b283bce6c426a1
Felipe Torres <felipe.torres21@hotmail.com>
2018-03-14 21:36:13 -0300

    agregando variables

--- src/practica/pico.java
+++ src/practica/pico.java
@@ -3,6 +3,11 @@ package practica;
 public class pico {
 
 	public static void main(String[] args) {
+		String nombre;
+		int	numero;
+		float pico;
+		
+			
 		
 		System.out.println("hola mundo");
 	}
